308b3778670d36add48a1be276a338c2676391dd
David Pam <niratpam@gmail.com>
2023-10-09 03:24:31 +0100

    Notes-App: Getting input from users with terminal args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,10 @@
 import chalk from 'chalk'
 import { getNotes } from './notes'
+
+const command = Bun.argv[2]
+
+if (command === 'add') {
+  console.log('Adding note!')
+} else if (command === 'remove' || 'rm') {
+  console.log('remove note!')
+}
