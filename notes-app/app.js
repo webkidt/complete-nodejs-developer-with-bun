ced3fb03a8d8b5c5b2b6171d99d39c070e185691
David Pam <niratpam@gmail.com>
2023-10-12 12:53:54 +0100

    Notes App: parsing command line arguments and creating command line commands with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,49 @@
 import chalk from 'chalk'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import { getNotes } from './notes'
 
-const command = Bun.argv[2]
+yargs()
+  .command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string',
+      },
+      body: {
+        describe: 'Content of your note',
+        demandOption: true,
+        type: 'string',
+      }
+    },
+    handler: function (argv) {
+      console.log(`Title: ${argv.title}\nBody: ${argv.body}`)
+    },
+  })
+  .command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+      console.log('Removing the note')
+    },
+  })
+  .command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+      console.log('Listing out all notes')
+    }
+  })
+  .command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+      console.log('Reading a note')
+    }
+  })
+  .version('1.1.0')
+  .parse(hideBin(Bun.argv))
 
-if (command === 'add') {
-  console.log('Adding note!')
-} else if (command === 'remove' || 'rm') {
-  console.log('remove note!')
-}
