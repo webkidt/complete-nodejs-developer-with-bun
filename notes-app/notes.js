a04ad4b160e6c15b74733203f653ab1c6faa51cf
David Pam <niratpam@gmail.com>
2023-10-17 03:03:03 +0100

    Notes-App: AddNote api using find instead of filter to check if note exists

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,9 +7,9 @@ const getNotes = () => {
 const addNote = async (title, body) => {
   const notes = await loadNotes()
 
-  const duplicateNotes = notes.filter((note) => note.title === title)
+  const duplicateNote = notes.find((note) => note.title === title)
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title,
       body,
