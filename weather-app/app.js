323fe0d8cdcb08a9cee8689f820c482f5277b54a
David Pam <niratpam@gmail.com>
2023-10-20 20:20:52 +0100

    Weather-App: get forecast location from command-line

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,28 @@
 import geocode from './utils/geocode'
 import forecast from './utils/forecast'
 
-// fetch coordinates for a given location
-geocode('Plateau Nigeria')
-  .then((geoData) => {
-    forecast(geoData.latitude, geoData.longitude)
-      .then((forecastData) => {
-        console.log(geoData.location)
-        console.log(forecastData)
+// get location from cli, fetch coordinates for the given location and get weather forecast for the location
+const getForecast = (arr) => {
+  if (arr < 3) {
+    console.error('Please provide a location')
+    return
+  } else {
+    const location = arr.slice(2).join('')
+    geocode(location)
+      .then((geoData) => {
+        forecast(geoData.latitude, geoData.longitude)
+          .then((forecastData) => {
+            console.log(geoData.location)
+            console.log(forecastData)
+          })
+          .catch((error) => {
+            console.error(error)
+          })
       })
       .catch((error) => {
-        console.log(error)
+        console.error(error)
       })
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+  }
+}
 
-
+getForecast(Bun.argv)
