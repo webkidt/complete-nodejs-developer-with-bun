03e868356eac2710323e18dac590717d36a5563d
David Pam <niratpam@gmail.com>
2023-11-15 23:05:56 +0100

    Web-Server: browser http request fetch, fetching forecast data of address from our server

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log('client side javascript file is loaded')
+
+try {
+  const res = await fetch('http://localhost:3000/weather?address=Boston')
+  const forecastData = await res.json()
+
+  if (forecastData.error) {
+    console.error(forecastData.error)
+  } else {
+    console.log(forecastData.location)
+    console.log(forecastData.forecast)
+  }
+
+
+} catch (error) {
+  console.error(error)
+}
