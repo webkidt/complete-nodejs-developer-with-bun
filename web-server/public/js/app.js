43f7cb52a685b66a4bad6307ad7c83859a554fdc
David Pam <niratpam@gmail.com>
2023-11-16 03:55:44 +0100

    Web-Server: setup UI for displaying location forecast results

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,22 +2,32 @@ console.log('client side javascript file is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit', async (e) => {
   e.preventDefault()
 
-  const location = search.value
+  const address = search.value
+
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+  messageOne.style.color = '#333333'
 
   try {
-    const res = await fetch(`http://localhost:3000/weather?address=${location}`)
+    const res = await fetch(`http://localhost:3000/weather?address=${address}`)
     const forecastData = await res.json()
 
     if (forecastData.error) {
+      messageOne.textContent = forecastData.error
+      messageOne.style.color = 'red'
       return console.error(forecastData.error)
     }
 
-    console.log(forecastData.location)
-    console.log(forecastData.forecast)
+    const { location, forecast } = forecastData
+    messageOne.textContent = location
+    messageTwo.textContent = forecast
+
   } catch (error) {
     console.error(error)
   }
