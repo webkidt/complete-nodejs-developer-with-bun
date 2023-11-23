5b77b51ee2ba65c67b62c2d9f2d36cc9ceb671f2
David Pam <niratpam@gmail.com>
2023-11-23 04:03:33 +0100

    Task-Manager: working with, and using mongodb Objectid

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,45 +1,51 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 
 const url = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const client = new MongoClient(url)
+const id = new ObjectId()
+console.log(id.id.length)
+console.log(id.toHexString().length)
+console.log(id.getTimestamp())
 
 try {
   await client.connect()
   const db = client.db(databaseName)
 
   // insert one document to users collection
-  // await db
+  // const insertOneResult = await db
   //   .collection('users')
   //   .insertOne({
-  //     name: 'David',
-  //     age: 27,
+  //     _id: id,
+  //     name: 'Joseph',
+  //     age: 26,
   //   })
   //   .catch((error) => {
   //     console.error(error)
   //   })
+  // console.log(insertOneResult)
 
   // insert many documents to tasks collection
-  await db
-    .collection('tasks')
-    .insertMany([
-      {
-        description: 'Clean the house',
-        completed: true,
-      },
-      {
-        description: 'Renew inspection',
-        completed: false,
-      },
-      {
-        description: 'Pot plants',
-        completed: false,
-      },
-    ])
-    .catch((error) => {
-      console.error(error)
-    })
+  // await db
+  //   .collection('tasks')
+  //   .insertMany([
+  //     {
+  //       description: 'Clean the house',
+  //       completed: true,
+  //     },
+  //     {
+  //       description: 'Renew inspection',
+  //       completed: false,
+  //     },
+  //     {
+  //       description: 'Pot plants',
+  //       completed: false,
+  //     },
+  //   ])
+  //   .catch((error) => {
+  //     console.error(error)
+  //   })
 } catch (error) {
   console.error(error)
 }
